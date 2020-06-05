e9eda578819e1e6486ba92aab363e6be8207652d
“acs107102” <acs107102@gm.ntcu.edu.tw>
2020-06-05 21:32:26 +0800

    Initial Commit

---
+++ FinalApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinalApp
+//
+//  Created by Winnie on 2020/6/5.
+//  Copyright © 2020 Winnie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
